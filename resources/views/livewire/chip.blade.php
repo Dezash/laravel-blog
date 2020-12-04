5d9843c93906cf99f55b3b1d9a171004933f6431
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2020-12-04 15:52:09 +0200

    Restyle chips

--- resources/views/livewire/chip.blade.php
+++ resources/views/livewire/chip.blade.php
@@ -1,22 +1,8 @@
-<div class="inline-flex items-center rounded-full bg-white border border-gray-200 p-px">
-    <div>
+<div class="inline-block">
+    <div class="chip">
         <span class="px-1 text-sm">{{ $title ?? "N/A" }}</span>
-        <button
-            type="button"
-            wire:click="$emit('keywordRemoved', '{{ $title }}')"
-            class="h-6 w-6 p-1 rounded-full bg-red-400 bg-opacity-25 focus:outline-none"
-        >
-        <svg
-            class="text-red-500 text-opacity-75"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-        >
-            <path
-            fill-rule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-            ></path>
+        <svg class="h-5 w-5 chipClose" wire:click="$emit('keywordRemoved', '{{ $title }}')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
         </svg>
-        </button>
     </div>
 </div>
