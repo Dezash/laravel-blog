06e1a4729cac0e56c8c58c5438997d109d2bd9de
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2020-12-06 04:46:04 +0200

    Register policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\Team;
 use App\Policies\TeamPolicy;
+use App\Models\Blog;
+use App\Policies\BlogPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,6 +17,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Team::class => TeamPolicy::class,
+        Blog::class => BlogPolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     /**
